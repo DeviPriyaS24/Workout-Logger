a70a59f697b68359f8425cc775670c2102f11724
DeviPriya <devipriyas.bca2020@gmail.com>
2025-05-10 21:50:58 +0530

    First commit: FitAhead a workout logger

---
+++ WorkoutLogger-Client/src/services/allAPI.js
@@ -0,0 +1,21 @@
+import ApiConfig from "./ApiConfig";
+
+export const AddPlansApi = async(PlansData)=>{
+    return await ApiConfig("post","/allPlans",PlansData)
+};
+
+export const getPlansApi = async()=>{
+    return await ApiConfig("get",'/allPlans',"")
+}
+
+export const deletePlansApi = async(planId)=>{
+    return await ApiConfig('delete',`/allPlans/${planId}`,{})
+}
+
+export const markAsCompletedApi = async (planId)=>{
+    return await ApiConfig('patch',`/allPlans/${planId}`,{ completed: true })
+}
+
+export const getCompletedPlansApi = async ()=>{
+    return await ApiConfig('get','/allPlans',"")
+}
